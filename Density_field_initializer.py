26d8d023f7eca15b96e28fbd6c35dc7767111f52
Matthew Turk <matthewturk@gmail.com>
2018-12-19 14:14:50 -0600

    Updating extensions and dependencies

--- Density_field_initializer.py
+++ Density_field_initializer.py
@@ -9,12 +9,8 @@ import h5py
 import time
 import density
 
-def run_generator(N = 256, seed=0x4d3d3d3,mu=1,sigma=0.1,
-                  output_fn = "den_init",binary=False):
+def run_generator(N = 256, seed=0x4d3d3d3,mu=1,sigma=0.1):
+    output_fn = "den_init.h5"
     den = density.Density_Generator((N,N,N),seed,mu,sigma)
-    if binary:
-        den.tofile(output_fn)
-
-    h=h5py.File(output_fn,'w')
-    h.create_dataset('dataset_2',data=den)
-    h.close()
+    with h5py.File("den_init.h5", 'w') as h:
+        h.create_dataset('density_dataset',data=den)
--- combine.py
+++ combine.py
@@ -1,19 +1,18 @@
 import h5py
 import numpy as np
 
-with h5py.File('vel_init','r') as f:
-    vel=f['/dataset_1'][:]
+with h5py.File('vel_init.h5','r') as f:
+    vel=f['/velocity_dataset'][:]
 
-with h5py.File('den_init','r') as f:
-    den=f['/dataset_2'][:]
+with h5py.File('den_init.h5','r') as f:
+    den=f['/density_dataset'][:]
 
 v1=vel[0]
 v2=vel[1]
 v3=vel[2]
 
-h=h5py.File('dataset','w')
-h.create_dataset('density',data=den)
-h.create_dataset('vx',data=v1)
-h.create_dataset('vy',data=v2)
-h.create_dataset('vz',data=v3)
-h.close()
+with h5py.File('dataset.h5','w') as h:
+    h.create_dataset('density',data=den)
+    h.create_dataset('velocity_x',data=v1)
+    h.create_dataset('velocity_y',data=v2)
+    h.create_dataset('velocity_z',data=v3)
