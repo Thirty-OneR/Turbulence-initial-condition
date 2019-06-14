ec8438602bdd6ec7b5985215e56ff6916b82855e
Thirty-OneR <suncyrus1997@gmail.com>
2019-06-14 09:58:19 -0500

    further change to the convert file

--- Turbulence_velocity_field_initializer.py
+++ Turbulence_velocity_field_initializer.py
@@ -11,7 +11,7 @@ import turbulence
 
 def run_generator(N = 256, index=3.0/2.0, seed=0x4d3d3d3):
     vel2 = turbulence.Turbulence_Generator((N,N,N),
-             index, 1, N**3, [0.0,0.0,0.0], [1.0/N, 1.0/N, 1.0/N],
+             index, 1/N**3, 10*N**3, [0.0,0.0,0.0], [1.0/N, 1.0/N, 1.0/N],
              seed)
     with h5py.File("vel_init.h5",'w') as h:
         h.create_dataset('velocity_dataset',data=vel2)
--- convert_to_gamer_format.py
+++ convert_to_gamer_format.py
@@ -10,18 +10,20 @@ for field in ["density", "velocity_x", "velocity_y", "velocity_z"]:
 
 #data['density'] -= (data['density'].min() * 1.001)
 data['density'] = (data['density']-data['density'].min())/(data['density'].max()-data['density'].min())
-data['density']=np.maximum(data['density'],1e-10)
+data['density']=np.maximum(data['density'],1e-16)
 
-data["vx2"] = data["velocity_x"] * (data["density"]**(-1/2))
-data["vy2"] = data["velocity_y"] * (data["density"]**(-1/2))
-data["vz2"] = data["velocity_z"] * (data["density"]**(-1/2))
+data["vx2"] = data["velocity_x"] #* (data["density"]**(-1/2))
+data["vy2"] = data["velocity_y"] #* (data["density"]**(-1/2))
+data["vz2"] = data["velocity_z"] #* (data["density"]**(-1/2))
 
 data["px"] = data["density"] * data["vx2"]
 data["py"] = data["density"] * data["vy2"]
 data["pz"] = data["density"] * data["vz2"]
 
-data["V"] = np.sqrt(data["vx2"]**2+data["vy2"]**2+data["vz2"]**2) 
-data["P"] = np.ones((64,64,64))
+N=64
+data["V"] = np.sqrt(data["vx2"]**2+data["vy2"]**2+data["vz2"]**2)
+data["P"] = np.sqrt(data["px"]**2+data["py"]**2+data["pz"]**2)
+#data["P"] = np.ones((N,N,N))
 
 data["Energy"] = (
                   0.5 * data["density"] * data["V"]**2 + 
